267cceccb10302dcc4e4d5e0e4db22f256d8d7a4
mlvpq <mlvpq@icloud.com>
2025-01-06 02:06:08 +0100

    add db context

---
+++ Ksiegarnia/Data/ApplicationDbContext.cs
@@ -0,0 +1,34 @@
+using Ksiegarnia.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ksiegarnia.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+        
+    }
+    
+    public DbSet<Book> Books { get; set; }
+    public DbSet<Author> Authors { get; set; }
+    public DbSet<Genre> Genres { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Book>()
+            .HasOne(b => b.Author)
+            .WithMany(a => a.Books)
+            .HasForeignKey(b => b.AuthorId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<Book>()
+            .HasOne(b => b.Genre)
+            .WithMany(g => g.Books)
+            .HasForeignKey(b => b.GenreId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
